34163c6f036a9b7dc70e62a4e2da84ee6825e65f
Leen Al-Ahmad <alahmadl@sheridancollege.ca>
2025-03-14 12:46:57 -0500

    Timing was  added for fps

--- GameController.cpp
+++ GameController.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 #include "SpriteSheet.h"
 #include "TTFont.h"
+#include "Timing.h"
 
 GameController::GameController()
 {
@@ -16,6 +17,7 @@ void GameController::RunGame()
 {
     AssetController::Instance().Initialize(10000000); // Allocate 10MB
     Renderer* r = &Renderer::Instance();
+    Timing* t = &Timing::Instance();
     r->Initialize(800, 600);
     
     Point ws = r->GetWindowSize();
@@ -31,8 +33,8 @@ void GameController::RunGame()
     SpriteSheet* sheet = SpriteSheet::Pool->GetResource();
     sheet->Load("./Assets/Textures/Warrior.tga");
     sheet->SetSize(17, 6, 69, 44);
-    sheet->AddAnimation(EN_AN_IDLE, 0, 6, 0.01f);
-    sheet->AddAnimation(EN_AN_RUN, 6, 8, 0.005f);
+    sheet->AddAnimation(EN_AN_IDLE, 0, 6, 6.0f);
+    sheet->AddAnimation(EN_AN_RUN, 6, 8, 6.0f);
 
     ofstream writeStream("resource.bin", ios::out | ios::binary);
     sheet->Serialize(writeStream);
@@ -55,17 +57,22 @@ void GameController::RunGame()
     // Main game loop
     while (m_sdlEvent.type != SDL_QUIT)
     {
+        t->Tick();
+
         SDL_PollEvent(&m_sdlEvent);
         r->SetDrawColor(Color(255, 255, 255, 255));
         r->ClearScreen();
-        r->RenderTexture(sheet2, sheet2->Update(EN_AN_IDLE), Rect(0, 0, 69 * 3, 44 * 3));
-        r->RenderTexture(sheet2, sheet2->Update(EN_AN_RUN), Rect(0, 150, 69 * 3, 150 + 44 * 3));
+        r->RenderTexture(sheet2, sheet2->Update(EN_AN_IDLE, t->GetDeltaTime()), Rect(0, 0, 69 * 3, 44 * 3));
+        r->RenderTexture(sheet2, sheet2->Update(EN_AN_RUN, t->GetDeltaTime()), Rect(0, 150, 69 * 3, 150 + 44 * 3));
 
-        std::string s = "Frame  number: " + std::to_string(sheet2->GetCurrentClip(EN_AN_IDLE));
+        std::string s = "Frame number: " + std::to_string(sheet2->GetCurrentClip(EN_AN_IDLE));
         font->Write(r->GetRenderer(),s.c_str(), SDL_Color{0, 255, 0}, SDL_Point{250, 50});
 
         s = "Frame number: " + std::to_string(sheet2->GetCurrentClip(EN_AN_RUN));
         font->Write(r->GetRenderer(), s.c_str(), SDL_Color{ 0, 255, 0 }, SDL_Point{ 250, 200 });
+
+        std::string fps = "Frames Per Secon: " + std::to_string(t->GetFPS());
+        font->Write(r->GetRenderer(), fps.c_str(), SDL_Color{ 0, 0, 255 }, SDL_Point{ 0, 0 });
 
         SDL_RenderPresent(r->GetRenderer());
     }
@@ -74,7 +81,7 @@ void GameController::RunGame()
     delete SpriteAnim::Pool;
     delete SpriteSheet::Pool;
 
-    //font->Shutdown();
+    font->Shutdown();
     r->Shutdown();
 }
 
